2b54a152ada2e5035a99a30e4ea670ecf62d928e
Rian445 <rianislam445@gmail.com>
2026-07-27 03:54:13 +0600

    chore: update MacAsc cask SHA256 and add documentation for Linux Mint port

--- Casks/macasc.rb
+++ Casks/macasc.rb
@@ -1,6 +1,6 @@
 cask "macasc" do
   version "1.0.0"
-  sha256 "0a5b6e9679f983ec94fc2dfa79c8c6717ce53cb6ef066b23914d2ced9fc10f06"
+  sha256 "8adc63070840d53b9a2acb0844351e910d6b107e8dc4be644a89c12c8e24e4e3"
 
   url "https://github.com/Rian445/MacAsc/releases/download/APP/Mac_ASC.dmg"
   name "Mac ASC"
